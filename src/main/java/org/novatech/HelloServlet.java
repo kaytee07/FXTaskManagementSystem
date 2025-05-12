a5331d3e7cd9b38c63ff0b68e56065245430250c
Kofi Taylor <kofitaylor@Kofis-MacBook-Air.local>
2025-05-12 21:29:12 +0000

    initial commit

---
+++ src/main/java/org/novatech/HelloServlet.java
@@ -0,0 +1,15 @@
+package org.novatech;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+import java.io.*;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse res)
+            throws IOException {
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+        out.println("<h1>Hello from Servlet!</h1>");
+    }
+}
