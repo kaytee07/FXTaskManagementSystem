6057b5a48bf1d4f87a8457ec14036922e3660ebc
kaytee07 <kofi.taylor07@gmail.com>
2025-05-13 03:24:31 +0000

    feat: add doGet method redirect http request and response to jsp page

--- src/main/java/org/novatech/servlets/AuthServlet.java
+++ src/main/java/org/novatech/servlets/AuthServlet.java
@@ -1,4 +1,33 @@
 package org.novatech.servlets;
 
-public class AuthServlet {
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.novatech.dao.UserDao;
+
+import javax.imageio.IIOException;
+import java.io.IOException;
+
+@WebServlet("/login")
+public class AuthServlet extends HttpServlet {
+    UserDao userDao = new UserDao();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/signin.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        try {
+
+        }
+
+        if(username != null && userDao.checkPasswd(password, user.))
+
+    }
 }
